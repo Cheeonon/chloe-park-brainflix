374de110ebd1aab9f66babf97dab71c735935391
채언 <Cheeon@Junghoonui-MacBookPro.local>
2022-08-21 11:20:13 -0400

    Add desktop breakpoint

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,15 @@
 import Comment from "../Comment/Comment"
 import CommentForm from "../CommentForm/CommentForm"
+import './Comments.scss';
 
 const Comments = () => {
   return (
-    <>
+    <div className="comments">
         <CommentForm />
         <Comment />
         <Comment />
         <Comment />
-    </>
+    </div>
   )
 }
 
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,19 +1,27 @@
 import Comments from '../Comments/Comments';
+import NextVideos from '../NextVideos/NextVideos';
 import VideoDescription from '../VideoDescription/VideoDescription';
 import './Video.scss';
 
 const Video = () => {
   return (
     <section className="video">
-        <div className="video__container">
+        <div className="video__video-container">
             <video 
                 poster="https://i.imgur.com/l2Xfgpl.jpg" 
                 controls
                 className="video__player"> 
             </video>    
         </div>
-        <VideoDescription />
-        <Comments />
+        <div className="video__content-container">
+          <div className="video__content">
+            <VideoDescription />
+            <Comments />
+          </div>
+          <div className="video__side">
+            <NextVideos />  
+          </div>
+        </div>
     </section>
   )
 }
